d27da129e103f29ce48731c3e63eec79bc4406a2
Josip <josip.naletilic@netformic.de>
2022-08-23 14:11:42 +0200

    implementing dimension and fact operator

--- plugins/operators/load_dimension.py
+++ plugins/operators/load_dimension.py
@@ -2,21 +2,51 @@ from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
+
 class LoadDimensionOperator(BaseOperator):
+    """
+    Class for loading and transforming data from redshift
+    to dimension table based on parameters
+
+    Attributes:
+        table: redshift table name
+        redshift_conn_id: redshift connection
+        sql_stmt: sql query
+        delete_table: if True data will be deleted before inserting
+    """
 
     ui_color = '#80BD9E'
+    insert_sql = """
+                INSERT INTO {}
+                {};
+            """
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 table="",
+                 redshift_conn_id="",
+                 sql_stmt="",
+                 delete_table=False,
                  *args, **kwargs):
-
         super(LoadDimensionOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.table = table
+        self.redshift_conn_id = redshift_conn_id
+        self.sql_stmt = sql_stmt
+        self.delete_table = delete_table
 
     def execute(self, context):
-        self.log.info('LoadDimensionOperator not implemented yet')
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+
+        if self.delete_table:
+            self.log.info("Deleting redshift table")
+            redshift.run("DELETE FROM {}".format(self.table))
+
+        self.log.info(f"Loading dimension table: {self.table}")
+        formatted_sql = LoadDimensionOperator.insert_sql.format(
+            self.table,
+            self.sql_stmt
+        )
+
+        self.log.info(f"Executing query: {formatted_sql}")
+        redshift.run(formatted_sql)
+        self.log.info(f"Load completed for table: {self.table}")
--- plugins/operators/load_fact.py
+++ plugins/operators/load_fact.py
@@ -3,20 +3,43 @@ from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
 class LoadFactOperator(BaseOperator):
+    """
+    Class for loading and transforming data from redshift
+    to fact tables based on parameters
+
+    Attributes:
+        table: redshift table name
+        redshift_conn_id: redshift connection
+        sql_stmt: sql query
+    """
 
     ui_color = '#F98866'
+    insert_sql = """
+            INSERT INTO {}
+            {};
+        """
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 table="",
+                 redshift_conn_id="",
+                 sql_stmt="",
                  *args, **kwargs):
 
         super(LoadFactOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.table = table
+        self.redshift_conn_id = redshift_conn_id
+        self.sql_stmt = sql_stmt
 
     def execute(self, context):
-        self.log.info('LoadFactOperator not implemented yet')
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+
+        self.log.info(f"Loading fact table: {self.table}")
+        formatted_sql = LoadFactOperator.insert_sql.format(
+            self.table,
+            self.sql_stmt
+        )
+
+        self.log.info(f"Executing query: {formatted_sql}")
+        redshift.run(formatted_sql)
+        self.log.info(f"Load completed for table: {self.table}")
